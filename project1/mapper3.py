8cf5b0a3dd9bd9f50cf9e64472d7879fa85713c6
Paul Isaacson <isaacspa8180@uwec.edu>
2018-09-21 22:14:45 -0500

    I did a rewrite of procject 1 #3. It's close. Needs further testing.

--- project1/mapper3.py
+++ project1/mapper3.py
@@ -1,14 +1,24 @@
 import sys 
 
 
+# def main(argv):
+#     for line in iter(sys.stdin.readline, ''):
+#         line = line.strip()
+#         me, friends = line.split(' : ', 1)
+#         print('{0}\t{1},{2}'.format(me, me, friends))
+#         for friend in friends.split(' '):
+#             print('{0}\t{1},{2}'.format(friend, me, friends))
+
+
 def main(argv):
     for line in iter(sys.stdin.readline, ''):
         line = line.strip()
-        k, vs = line.split(' : ', 1)
-        for v in vs.split(' '):
-            l = min(int(k), int(v))
-            h = max(int(k), int(v))
-            print('{0},{1}\t{2}'.format(l, h, vs))        
+        me, friends = line.split(' : ', 1)
+        print('{0}\t{1},{2}'.format(me, me, friends))
+        list_friends = friends.split(' ')
+        for friend in list_friends:
+            list_friends.remove(friend)
+            print('{0}\t{1},{2}'.format(friend, me, ' '.join(list_friends)))
 
 
 if __name__ == '__main__':
--- project1/reducer3.py
+++ project1/reducer3.py
@@ -1,16 +1,58 @@
 import sys 
-from collections import defaultdict
+from collections import Counter, defaultdict
+
+
+# def main(argv):
+#     d = defaultdict(list)
+#     for line in iter(sys.stdin.readline, ''):
+#         line = line.strip()
+#         k, vs = line.split('\t', 1)
+#         vs = vs.split(' ')
+#         vs = {int(v) for v in vs}
+#         d[k].append(vs)
+#     print(d)
 
 
 def main(argv):
-    d = defaultdict(list)
+    all_friend_friends = defaultdict(list)
+    my_friends = defaultdict(set)
     for line in iter(sys.stdin.readline, ''):
         line = line.strip()
-        k, vs = line.split('\t', 1)
-        vs = vs.split(' ')
-        vs = {int(v) for v in vs}
-        d[k].append(vs)
-    print(d)
+        me, network = line.split('\t', 1)
+        friend, friend_friends = network.split(',')
+        friend_friends = set(friend_friends.split(' '))
+        if me == friend:
+            my_friends[me] = friend_friends
+        else:
+            all_friend_friends[me].append(friend_friends)
+
+    not_my_friends = defaultdict(list)
+    for me, list_friend_friends in all_friend_friends.items():
+        for friend_friends in list_friend_friends:
+            not_my_friends[me].append(friend_friends.difference(my_friends[me]))
+
+    count_not_my_friends = defaultdict(list)        
+    for me, list_friend_friends in not_my_friends.items():
+        flat_list = []
+        for friend_friends in list_friend_friends:
+            for friend in friend_friends:
+                flat_list.append(friend)
+        count_not_my_friends[me] = Counter(flat_list)
+    print(count_not_my_friends)
+
+    might_know = defaultdict(list)
+    for me, not_friends in count_not_my_friends.items():
+        for not_friend, cnt in not_friends.items():
+            if cnt >= 2:
+                might_know[me].append(not_friend)
+    # print(might_know)
+
+
+    # prob_know = defaultdict(list)
+    # for not_friend, cnt in count_not_my_friends.items():
+    #     if int(cnt) > 4:
+    #         prob_know[me].append(not_friend)
+
 
 if __name__ == '__main__':
     main(sys.argv)
